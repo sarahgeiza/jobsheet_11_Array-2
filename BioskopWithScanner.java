33bf184d515f913b5d4e81e8d70d1ab25ccfca2e
sarahgeiza <sarahgeizask@gmail.com>
2025-11-14 22:43:08 +0700

    Percobaan 2 Pertanyaan 4

--- BioskopWithScanner.java
+++ BioskopWithScanner.java
@@ -3,7 +3,7 @@ public class BioskopWithScanner {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-       String[][] penonton = new String[4][2];  // 4 baris, 2 kolom
+        String[][] penonton = new String[4][2];
         int menu;
         int baris, kolom;
         String nama;
@@ -15,40 +15,53 @@ public class BioskopWithScanner {
             System.out.println("3. Exit");
             System.out.print("Pilih menu: ");
             menu = sc.nextInt();
-            sc.nextLine(); // buang newline dari nextInt()
+            sc.nextLine();
 
             if (menu == 1) {
-                // INPUT DATA PENONTON
+
                 System.out.print("Masukkan nama: ");
                 nama = sc.nextLine();
 
-                System.out.print("Masukkan baris (1-4): ");
-                baris = sc.nextInt();
+                while (true) {
+                    System.out.print("Masukkan baris (1-4): ");
+                    baris = sc.nextInt();
+                    System.out.print("Masukkan kolom (1-2): ");
+                    kolom = sc.nextInt();
+                    sc.nextLine();
 
-                System.out.print("Masukkan kolom (1-2): ");
-                kolom = sc.nextInt();
-                sc.nextLine(); // buang newline
+                    if (baris < 1 || baris > 4 || kolom < 1 || kolom > 2) {
+                        System.out.println("Nomor kursi tidak ada. Coba lagi.");
+                        continue;
+                    }
 
-                // Simpan data ke array
-                penonton[baris - 1][kolom - 1] = nama;
-                System.out.println("Data penonton berhasil disimpan!");
+                    if (penonton[baris - 1][kolom - 1] != null) {
+                        System.out.println("Kursi ini sudah terisi. Pilih kursi lain.");
+                    } else {
+                        penonton[baris - 1][kolom - 1] = nama;
+                        System.out.println("Data penonton berhasil disimpan!");
+                        break;
+                    }
+                }
 
             } else if (menu == 2) {
-                // TAMPILKAN DAFTAR PENONTON
+
                 System.out.println("\n DAFTAR PENONTON ");
                 for (int i = 0; i < 4; i++) {
                     for (int j = 0; j < 2; j++) {
+                        String isiKursi;
+
                         if (penonton[i][j] == null) {
-                            System.out.print("[Kosong]\t");
+                            isiKursi = "[Kosong]";
                         } else {
-                            System.out.print("[" + penonton[i][j] + "]\t");
+                            isiKursi = "[" + penonton[i][j] + "]";
                         }
+
+                        System.out.printf("%-12s", isiKursi);
                     }
                     System.out.println();
                 }
 
             } else if (menu == 3) {
-                // EXIT PROGRAM
                 System.out.println("Program selesai.");
                 break;
 
